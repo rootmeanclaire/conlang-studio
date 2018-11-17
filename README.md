8e022cd38a64be14fdee15ad7ef9ffcdd5353b5f
Evan Shimoniak <eshimoniak@gmail.com>
2018-11-16 17:13:55 -0800

    Stuff I forgot to commit last time

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 Conlang Studio
 ==============
 
+Markdown
+--------
+
+In addition to the standard markdown, Conlang Studio also supports some common extensions such as tables, autolink, and strikethrough.
+
+Addionally, it supports a custom escape to convert a list of pulmonic consonants into a table
+
+So this:
+
+	\ipaChart: d, t, ɾ, b
+
+Will be converted to this:
+	
+||Alveolar|Bilabial
+|:---|:---:|:---:
+|**Plosive**|t, d|b
+|**Tap**|ɾ|
 
 
 Open Source Libraries
--- src/com/eshimoniak/conlangstudio/ui/panels/editor/RawEditor.java
+++ src/com/eshimoniak/conlangstudio/ui/panels/editor/RawEditor.java
@@ -2,12 +2,25 @@ package com.eshimoniak.conlangstudio.ui.panels.editor;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.Document;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
 /**
  * A simple monospaced text editor
@@ -17,6 +30,7 @@ public class RawEditor extends JPanel {
 	private JScrollPane scroller;
 	private JTextArea textArea;
 	private boolean matchesFile = true;
+	private UndoManager undoManager;
 	
 	public RawEditor() {
 		super(new BorderLayout());
@@ -40,6 +54,46 @@ public class RawEditor extends JPanel {
 		});
 		scroller = new JScrollPane(textArea);
 		add(scroller, BorderLayout.CENTER);
+		
+		undoManager = new UndoManager();
+		Document doc = textArea.getDocument();
+		doc.addUndoableEditListener(new UndoableEditListener() {
+			@Override
+			public void undoableEditHappened(UndoableEditEvent e) {
+				undoManager.addEdit(e.getEdit());
+			}
+		});
+		
+		InputMap im = textArea.getInputMap(JComponent.WHEN_FOCUSED);
+		ActionMap am = textArea.getActionMap();
+		
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_Z, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), "Undo");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_Y, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), "Redo");
+		
+		am.put("Undo", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (undoManager.canUndo()) {
+						undoManager.undo();
+					}
+				} catch (CannotUndoException exp) {
+					exp.printStackTrace();
+				}
+			}
+		});
+		am.put("Redo", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (undoManager.canRedo()) {
+						undoManager.redo();
+					}
+				} catch (CannotUndoException exp) {
+					exp.printStackTrace();
+				}
+			}
+		});
 	}
 	
 	public void setText(String str) {
